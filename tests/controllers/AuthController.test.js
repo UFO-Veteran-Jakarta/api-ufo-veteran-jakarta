6bc727eefdc12f1a6f634133c8756d6f0090a6b1
ashafaatadhis <skullreaper387@gmail.com>
2024-07-18 19:33:01 +0700

    test: create testing for register

---
+++ tests/controllers/AuthController.test.js
@@ -0,0 +1,77 @@
+const request = require("supertest");
+const app = require("../../src/app");
+
+describe("Auth Controller", () => {
+  describe("POST /api/v1/register", () => {
+    it("should be rejected if method http is not POST", async () => {
+      const data = {
+        username: "admin",
+        password: "Ad@123",
+      };
+      const res = await request(app).put("/api/v1/register").send(data);
+
+      expect(res.statusCode).toEqual(405);
+      expect(res.body.status).toEqual(405);
+      expect(res.body.message).toBe("Wrong method");
+    });
+    it("should be rejected if password length not in requirement", async () => {
+      const data = {
+        username: "admin",
+        password: "Ad@123",
+      };
+      const res = await request(app).post("/api/v1/register").send(data);
+
+      expect(res.statusCode).toEqual(500);
+      expect(res.body.status).toEqual(500);
+      expect(res.body.message).toBe(
+        "Password length must be a minimum of 8 characters and a maximum of 64 characters"
+      );
+    });
+    it("should be rejected if password not valid in validate", async () => {
+      const data = {
+        username: "admin",
+        password: "Admin12345678",
+      };
+      const res = await request(app).post("/api/v1/register").send(data);
+
+      expect(res.statusCode).toEqual(500);
+      expect(res.body.status).toEqual(500);
+      expect(res.body.message).toBe(
+        "Password must consist of at least one lowercase, one uppercase, one special character, and one number"
+      );
+    });
+    it("should be rejected if username is exist", async () => {
+      const data = {
+        username: "admin",
+        password: "Admin@12345",
+      };
+      const res = await request(app).post("/api/v1/register").send(data);
+
+      expect(res.statusCode).toEqual(500);
+      expect(res.body.status).toEqual(500);
+      expect(res.body.message).toBe("Username already taken");
+    });
+    // it("should be rejected if registration fails", async () => {
+    //   const data = {
+    //     username: "admin",
+    //     password: "Admin@12345678",
+    //   };
+    //   const res = await request(app).post("/api/v1/register").send(data);
+
+    //   expect(res.statusCode).toEqual(500);
+    //   expect(res.body.status).toEqual(500);
+    //   expect(res.body.message).toBe("Failed to register new user!");
+    // });
+    it("should be able to register", async () => {
+      const data = {
+        username: "admin",
+        password: "Admin@12345",
+      };
+      const res = await request(app).post("/api/v1/register").send(data);
+
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.status).toEqual(200);
+      expect(res.body.message).toBe("Successfully registered new user!");
+    });
+  });
+});
