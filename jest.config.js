7156b2c78fd9b2f8c3fe904d4248a29dfa84c7b2
ashafaatadhis <skullreaper387@gmail.com>
2024-07-21 03:28:51 +0700

    fix: fixing relative import

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   testEnvironment: "node",
   setupFilesAfterEnv: ["./tests/setup/setupTestEnvironment.js"],
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-  },
+
+  moduleDirectories: ["node_modules", "src"],
 };
