4c914f9657d213edfd4539f37f5638efa62898b7
ashafaatadhis <skullreaper387@gmail.com>
2024-07-21 03:30:28 +0700

    fix: fixing relative import

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const authController = require("src/controllers/authController");
+const authController = require("controllers/authController");
 
 const {
   registerValidationRules,
