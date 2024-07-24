008968d9060f9f5f8f1c0525bff5081972f96f02
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-24 08:55:09 +0700

    refactor: Update multer configuration to allow only WEBP image files

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,28 +1,11 @@
-import multer from "multer";
-import path from "path";
-
-// const diskStorage = multer.diskStorage({
-//   destination: path.resolve(__dirname, "../", "public/uploads"),
-//   filename(
-//     req: Express.Request,
-//     file: Express.Multer.File,
-//     fn: (error: Error | null, filename: string) => void
-//   ): void {
-//     fn(
-//       null,
-//       `${new Date().getTime().toString()}-${file.fieldname}${path.extname(
-//         file.originalname
-//       )}`
-//     );
-//   },
-// });
+const multer = require("multer");
+const path = require("path");
 
 const storage = multer.memoryStorage();
 
 module.exports = multer({
   storage,
   fileFilter(req, file, callback) {
-    console.log("JANCOOOK");
     const extension =
       [".webp"].indexOf(path.extname(file.originalname).toLowerCase()) >= 0;
     const mimeType = ["image/webp"].indexOf(file.mimetype) >= 0;
@@ -33,7 +16,7 @@ module.exports = multer({
 
     callback(
       new Error(
-        "Invalid file type. Only picture file on type PNG and JPG are allowed!"
+        "Invalid file type. Only picture file on type WEBP are allowed!"
       )
     );
   },
