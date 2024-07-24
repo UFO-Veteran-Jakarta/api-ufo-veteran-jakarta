ee1a99d32111b345ae20a000f88e4ef4fa194056
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-24 15:10:21 +0700

    chore: Add error handling for multer file upload in app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const routes = require("./routes");
 const cookieParser = require("cookie-parser");
 const migrate = require("./migration/users");
 const limiter = require("./utils/limiter");
+const multer = require("multer");
 // const pool = require("./config/database");
 
 const app = express();
@@ -14,5 +15,14 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(limiter);
 app.use("/api/v1", routes);
+app.use(function (err, req, res, next) {
+  if (err instanceof multer.MulterError) {
+    res.status(500).json({ error: err.message });
+  } else if (err) {
+    res.status(500).json({ error: err.message });
+  } else {
+    next();
+  }
+});
 
 module.exports = app;
