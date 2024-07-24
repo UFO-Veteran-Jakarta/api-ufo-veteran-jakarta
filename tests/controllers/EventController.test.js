ecf51c9f0f1c9c4d8842e3d12f78e91b25ef0c71
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-24 08:55:14 +0700

    feat: Add validation for image file format in EventController test

--- tests/controllers/EventController.test.js
+++ tests/controllers/EventController.test.js
@@ -1,6 +1,7 @@
 const { deleteUserByUsername } = require("src/models/userModel");
 const request = require("supertest");
 const app = require("src/app");
+const path = require("path");
 
 describe("Event Controller", () => {
   beforeEach(async () => {
@@ -18,6 +19,7 @@ describe("Event Controller", () => {
       expect(res.statusCode).toEqual(401);
       expect(res.body.status).toEqual(401);
     });
+
     it("should be rejected if cover and cover landscape not exist", async () => {
       const data = {
         username: "admin",
@@ -26,20 +28,21 @@ describe("Event Controller", () => {
       await request(app).post("/api/v1/register").send(data);
 
       const login = await request(app).post("/api/v1/login").send(data);
-      const filePath = "../test.jpg";
       const res = await request(app)
         .post("/api/v1/events")
         .set("Cookie", `token=${login.body.authorization.token}`)
         .set("Authorization", `Bearer ${login.body.authorization.token}`)
-        .attach("file", filePath);
+        .send();
+
       console.log(res.body);
 
-      expect(res.statusCode).toEqual(500);
-      expect(res.body.status).toEqual(500);
+      expect(res.statusCode).toEqual(400);
+      expect(res.body.status).toEqual(400);
       expect(res.body.errors[0].msg).toBe(
         "Cover and Cover Landscape Requirements"
       );
     });
+
     it("should be rejected if cover and cover landscape is not webp", async () => {
       const data = {
         username: "admin",
@@ -48,20 +51,21 @@ describe("Event Controller", () => {
       await request(app).post("/api/v1/register").send(data);
 
       const login = await request(app).post("/api/v1/login").send(data);
-      const filePath = "../test.jpg";
+      const filePath = path.resolve(__dirname, "../test.jpg");
       const res = await request(app)
         .post("/api/v1/events")
         .set("Cookie", `token=${login.body.authorization.token}`)
         .set("Authorization", `Bearer ${login.body.authorization.token}`)
-        .attach("file", filePath);
+        .attach("cover", filePath);
 
-      expect(res.statusCode).toEqual(500);
-      expect(res.body.status).toEqual(500);
+      expect(res.statusCode).toEqual(400);
+      expect(res.body.status).toEqual(400);
       expect(res.body.errors[0].msg).toBe(
         "Cover/Cover Landscape must be in WEBP Format"
       );
     });
-    it("should be accept if cover and cover landscape is less than 500kb", async () => {
+
+    it("should be accepted if cover and cover landscape is less than 500kb", async () => {
       const data = {
         username: "admin",
         password: "Admin@12345",
@@ -69,12 +73,12 @@ describe("Event Controller", () => {
       await request(app).post("/api/v1/register").send(data);
 
       const login = await request(app).post("/api/v1/login").send(data);
-      const filePath = "../test.webp";
+      const filePath = path.resolve(__dirname, "../test.webp");
       const res = await request(app)
         .post("/api/v1/events")
         .set("Cookie", `token=${login.body.authorization.token}`)
         .set("Authorization", `Bearer ${login.body.authorization.token}`)
-        .attach("file", filePath);
+        .attach("cover", filePath);
 
       expect(res.statusCode).toEqual(200);
       expect(res.body.status).toEqual(200);
