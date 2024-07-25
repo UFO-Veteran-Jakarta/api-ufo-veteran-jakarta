80546ef5999129659c2f241729aae987a18d27f4
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-26 06:14:05 +0700

    refactor: Add getAllEvents endpoint to eventController

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -1,6 +1,6 @@
 const logger = require("../utils/logger");
 const { sendResponse } = require("../helpers/response");
-const { addEvent } = require("../services/eventService");
+const { addEvent, getAllEvents } = require("../services/eventService");
 const { generateUniqueSlug } = require("../helpers/slug");
 
 exports.uploadEvent = async (req, res) => {
@@ -15,5 +15,16 @@ exports.uploadEvent = async (req, res) => {
   } catch (error) {
     logger.error("Add Error: Failed Add Event");
     return sendResponse(res, 500, error.message);
+  }
+};
+
+exports.getAllEvents = async (req, res) => {
+  try {
+    const events = await getAllEvents();
+    logger.info("Successfully Get All Events");
+    return sendResponse(res, 200, "Successfully Get All Events", events);
+  } catch (error) {
+    logger.error("Failed to Get All Events");
+    return sendResponse(res, 500, "Failed to Get All Events");
   }
 };
