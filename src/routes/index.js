d3917ccadf8f5dd58b25046fb44d8ebc93b4d321
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-28 00:08:17 +0700

    feat: Add partnerRoutes module for handling partner-related routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const authRoutes = require("./authRoutes");
 const contentRoutes = require("./contentRoutes");
 const eventRoutes = require("./eventRoutes");
+const partnerRoutes = require("./partnerRoutes");
 
 const router = express.Router();
 
 router.use("/", authRoutes);
 router.use("/contents", contentRoutes);
 router.use("/events", eventRoutes);
+router.use("/partners", partnerRoutes);
 
 module.exports = router;
