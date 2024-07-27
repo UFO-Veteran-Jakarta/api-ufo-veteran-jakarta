d6e42dd1d747e2ff65f279ac57d5c5a25595c9cc
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-28 06:51:17 +0700

    refactor: remove duplicated lines and make function for the duplicated lines.

--- tests/controllers/PartnerController.test.js
+++ tests/controllers/PartnerController.test.js
@@ -5,81 +5,66 @@ const path = require("path");
 const fs = require("fs");
 
 describe("Partner Controller", () => {
+  let data;
+  let login;
+
+  const registerAndLogin = async () => {
+    await request(app).post("/api/v1/register").send(data);
+    login = await request(app).post("/api/v1/login").send(data);
+  };
+
+  const postPartners = async (logoPath) => {
+    if (logoPath && !fs.existsSync(logoPath)) {
+      throw new Error(`File does not exist: ${logoPath}`);
+    }
+
+    let req = request(app)
+      .post("/api/v1/partners")
+      .set("Cookie", `token=${login.body.authorization.token}`)
+      .set("Authorization", `Bearer ${login.body.authorization.token}`);
+
+    if (logoPath) {
+      req = req.attach("logo", logoPath);
+    }
+
+    return await req;
+  };
+
   beforeEach(async () => {
     await deleteUserByUsername("admin");
+    data = {
+      username: "admin",
+      password: "Admin@12345",
+    };
+    await registerAndLogin();
   });
 
   describe("POST /api/v1/partner", () => {
     it("should be rejected if user not authenticated", async () => {
       const res = await request(app).post("/api/v1/partners");
-
       expect(res.statusCode).toEqual(401);
       expect(res.body.status).toEqual(401);
       expect(res.body.message).toBeDefined();
     });
 
     it("logo should be provided", async () => {
-      const data = {
-        username: "admin",
-        password: "Admin@12345",
-      };
-      await request(app).post("/api/v1/register").send(data);
-
-      const login = await request(app).post("/api/v1/login").send(data);
-
-      const res = await request(app)
-        .post("/api/v1/partners")
-        .set("Cookie", `token=${login.body.authorization.token}`)
-        .set("Authorization", `Bearer ${login.body.authorization.token}`);
-
+      const res = await postPartners();
       expect(res.statusCode).toEqual(500);
       expect(res.body.status).toEqual(500);
       expect(res.body.message).toBe("Partner logo are required");
     });
 
     it("logo should be webp", async () => {
-      const data = {
-        username: "admin",
-        password: "Admin@12345",
-      };
-
-      await request(app).post("/api/v1/register").send(data);
-
-      const login = await request(app).post("/api/v1/login").send(data);
       const filePathLogo = path.resolve(__dirname, "../test.jpg");
-
-      const res = await request(app)
-        .post("/api/v1/partners")
-        .set("Cookie", `token=${login.body.authorization.token}`)
-        .set("Authorization", `Bearer ${login.body.authorization.token}`)
-        .attach("logo", filePathLogo);
-
+      const res = await postPartners(filePathLogo);
       expect(res.statusCode).toEqual(500);
       expect(res.body.status).toEqual(500);
       expect(res.body.message).toBe("Partner logo must be in WEBP format");
     });
 
     it("should be accept if logo partner is less than 500kb", async () => {
-      const data = {
-        username: "admin",
-        password: "Admin@12345",
-      };
-      await request(app).post("/api/v1/register").send(data);
-
-      const login = await request(app).post("/api/v1/login").send(data);
-
       const filePathLogo = path.resolve(__dirname, "../tes-large.webp");
-
-      if (!fs.existsSync(filePathLogo)) {
-        throw new Error(`File does not exist: ${filePathLogo}`);
-      }
-
-      const res = await request(app)
-        .post("/api/v1/partners")
-        .set("Cookie", `token=${login.body.authorization.token}`)
-        .set("Authorization", `Bearer ${login.body.authorization.token}`)
-        .attach("logo", filePathLogo);
-
+      const res = await postPartners(filePathLogo);
       expect(res.statusCode).toEqual(500);
       expect(res.body.status).toEqual(500);
       expect(res.body.message).toBe(
@@ -88,20 +73,7 @@ describe("Partner Controller", () => {
     }, 60000);
 
     it("should be upload partner logo", async () => {
-      const data = {
-        username: "admin",
-        password: "Admin@12345",
-      };
-      await request(app).post("/api/v1/register").send(data);
-
-      const login = await request(app).post("/api/v1/login").send(data);
-
       const filePathLogo = path.resolve(__dirname, "../test-small.webp");
-
-      if (!fs.existsSync(filePathLogo)) {
-        throw new Error(`File does not exist: ${filePathLogo}`);
-      }
-
       const partnerData = {
         name: "Test Partner",
         logo: filePathLogo,
@@ -118,9 +90,5 @@ describe("Partner Controller", () => {
       expect(res.body.status).toEqual(200);
       expect(res.body.message).toBe("Successfully Add New Partner");
     });
-  });
-
-  describe("GET /api/v1/partners", () => {
-    console.log("Halo ini test");
   });
 });
