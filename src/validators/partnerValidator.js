119d8cbf3ef478ba3a5a1bc70b099e6fc1ae65ed
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-27 22:06:04 +0700

    feat: Add partnerValidator module for partner validation

---
+++ src/validators/partnerValidator.js
@@ -0,0 +1,27 @@
+const { check, validationResult } = require("express-validator");
+
+const postPartnerValidationRules = () => {
+  return [
+    check("name")
+      .isString()
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage("partner name is required")
+      .isLength({ max: 255 })
+      .withMessage("Partner name must be no more than 255 characters"),
+  ];
+};
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(500).json({ status: 500, errors: errors.array() });
+  }
+  next();
+};
+
+module.exports = {
+  postPartnerValidationRules,
+  validate,
+};
