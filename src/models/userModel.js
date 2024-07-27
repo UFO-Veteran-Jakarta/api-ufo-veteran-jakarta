7f8649612a95f1c8e425589043d80d22a466751e
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-27 20:45:10 +0700

    Update userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,5 +1,6 @@
 const pool = require("../config/database");
 const { hash } = require("../helpers/bcrypt");
+const { compare } = require("../helpers/bcrypt");
 
 exports.getAllUser = async () => {
   const res = await pool.query(
