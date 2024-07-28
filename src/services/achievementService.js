d8e3fc526ed03b8582f431b4d91258cea46ae64b
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-28 20:37:02 +0700

    feat: Update achievementService to include getAllAchievements function

--- src/services/achievementService.js
+++ src/services/achievementService.js
@@ -1,5 +1,17 @@
-const { addAchievement } = require("../models/achievementsModel");
+const {
+  addAchievement,
+  getAllAchievements,
+} = require("../models/achievementsModel");
 
 exports.addAchievemnt = async (data) => {
   return await addAchievement(data);
+};
+
+exports.getAllAchievements = async () => {
+  try {
+    return await getAllAchievements();
+  } catch (error) {
+    console.error("Error fetching achievements:", error);
+    throw error;
+  }
 };
