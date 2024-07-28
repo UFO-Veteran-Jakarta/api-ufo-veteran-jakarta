0f548c8dab446786b70b2db1042b5f70cb1fa008
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-28 09:54:08 +0700

    feat: Add DELETE /api/v1/partners endpoint to delete partner by id

--- tests/controllers/PartnerController.test.js
+++ tests/controllers/PartnerController.test.js
@@ -181,4 +181,52 @@ describe("Partner Controller", () => {
       expect(res.body.message).toEqual("Successfully Update Partner");
     });
   });
+
+  describe("DELET /api/v1/partners?id=id_partner", () => {
+    it("should be rejected if user not authenticated", async () => {
+      const res = await request(app).post("/api/v1/partners");
+      expect(res.statusCode).toEqual(401);
+      expect(res.body.status).toEqual(401);
+      expect(res.body.message).toBeDefined();
+    });
+
+    it("should return 404 if partner not found", async () => {
+      const updateData = {
+        name: "Tes update partner",
+      };
+
+      const res = await request(app)
+        .put(`/api/v1/partners?id=123123`)
+        .set("Cookie", `token=${login.body.authorization.token}`)
+        .set("Authorization", `Bearer ${login.body.authorization.token}`)
+        .send(updateData);
+
+      expect(res.statusCode).toEqual(404);
+      expect(res.body.message).toEqual("Partner not found");
+    });
+
+    it("should be delete partner", async () => {
+      const filePathLogo = path.resolve(__dirname, "../test-small.webp");
+      const partnerData = {
+        name: "Test Partner",
+        logo: filePathLogo,
+      };
+
+      const partner = await request(app)
+        .post("/api/v1/partners")
+        .set("Cookie", `token=${login.body.authorization.token}`)
+        .set("Authorization", `Bearer ${login.body.authorization.token}`)
+        .field("name", partnerData.name)
+        .attach("logo", partnerData.logo);
+
+      const res = await request(app)
+        .delete(`/api/v1/partners?id=${partner.body.data.id}`)
+        .set("Cookie", `token=${login.body.authorization.token}`)
+        .set("Authorization", `Bearer ${login.body.authorization.token}`);
+
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.status).toEqual(200);
+      expect(res.body.message).toEqual("Successfully Delete Partner");
+    });
+  });
 });
