46fc35868c2cf4a61470d9a9f7a2ab83623fddb3
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-28 17:03:49 +0700

    feat: Add achievementRoutes for handling achievement-related requests

---
+++ src/routes/achievementRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const achievementController = require("../controllers/achievementController");
+
+const {
+  postAchievementValidationRules,
+  validate,
+} = require("../validators/achievementValidator");
+
+const { authentication } = require("../middlewares/authMiddleware");
+const {
+  checkFile,
+  checkUpdateFile,
+} = require("../middlewares/achievementMiddlewareFile");
+
+const router = express.Router();
+
+router.post(
+  "/",
+  authentication(),
+  checkFile("logo"),
+  postAchievementValidationRules(),
+  validate,
+  achievementController.addAchievement
+);
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const authRoutes = require("./authRoutes");
 const contentRoutes = require("./contentRoutes");
 const eventRoutes = require("./eventRoutes");
 const partnerRoutes = require("./partnerRoutes");
+const achievementRoutes = require("./achievementRoutes");
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use("/", authRoutes);
 router.use("/contents", contentRoutes);
 router.use("/events", eventRoutes);
 router.use("/partners", partnerRoutes);
+router.use("/achievements", achievementRoutes);
 
 module.exports = router;
