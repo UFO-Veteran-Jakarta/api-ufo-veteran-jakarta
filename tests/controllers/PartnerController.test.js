eaa28968e241e9ba9baea1ce93a5558bfee16b91
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-28 09:39:55 +0700

    test: create testing to update partner data

--- tests/controllers/PartnerController.test.js
+++ tests/controllers/PartnerController.test.js
@@ -101,4 +101,84 @@ describe("Partner Controller", () => {
       expect(Array.isArray(res.body.data)).toBe(true);
     });
   });
+
+  describe("GET /api/v1/partners/?id=id_partner", () => {
+    it("should be return partner by id", async () => {
+      const filePathLogo = path.resolve(__dirname, "../test-small.webp");
+      const partnerData = {
+        name: "Test Partner",
+        logo: filePathLogo,
+      };
+
+      const partner = await request(app)
+        .post("/api/v1/partners")
+        .set("Cookie", `token=${login.body.authorization.token}`)
+        .set("Authorization", `Bearer ${login.body.authorization.token}`)
+        .field("name", partnerData.name)
+        .attach("logo", partnerData.logo);
+
+      const res = await request(app).get(
+        `/api/v1/partners/?id=${partner.body.data.id}`
+      );
+
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.status).toEqual(200);
+      expect(res.body.message).toEqual("Successfully Get Partner");
+      expect(res.body.data).toBeDefined();
+      expect(res.body.data.slug).toBe(partner.body.data.slug);
+    });
+  });
+
+  describe("PUT /api/v1/partners?id=id_partner", () => {
+    it("should be rejected if user not authenticated", async () => {
+      const res = await request(app).post("/api/v1/partners");
+      expect(res.statusCode).toEqual(401);
+      expect(res.body.status).toEqual(401);
+      expect(res.body.message).toBeDefined();
+    });
+
+    it("should return 404 if partner not found", async () => {
+      const updateData = {
+        name: "Tes update partner",
+      };
+
+      const res = await request(app)
+        .put(`/api/v1/partners?id=123123`)
+        .set("Cookie", `token=${login.body.authorization.token}`)
+        .set("Authorization", `Bearer ${login.body.authorization.token}`)
+        .send(updateData);
+
+      expect(res.statusCode).toEqual(404);
+      expect(res.body.message).toEqual("Partner not found");
+    });
+
+    it("should be update partner", async () => {
+      const filePathLogo = path.resolve(__dirname, "../test-small.webp");
+      const partnerData = {
+        name: "Test Partner",
+        logo: filePathLogo,
+      };
+
+      const partner = await request(app)
+        .post("/api/v1/partners")
+        .set("Cookie", `token=${login.body.authorization.token}`)
+        .set("Authorization", `Bearer ${login.body.authorization.token}`)
+        .field("name", partnerData.name)
+        .attach("logo", partnerData.logo);
+
+      const updateData = {
+        name: "Tes update partner",
+      };
+
+      const res = await request(app)
+        .put(`/api/v1/partners?id=${partner.body.data.id}`)
+        .set("Cookie", `token=${login.body.authorization.token}`)
+        .set("Authorization", `Bearer ${login.body.authorization.token}`)
+        .send(updateData);
+
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.status).toEqual(200);
+      expect(res.body.message).toEqual("Successfully Update Partner");
+    });
+  });
 });
