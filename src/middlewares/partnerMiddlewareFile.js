3cee3a0deb079eeb4723b577ec90b7bc888cf6f3
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-28 09:40:28 +0700

    feat: Add file format and size validation to partnerMiddlewareFile

--- src/middlewares/partnerMiddlewareFile.js
+++ src/middlewares/partnerMiddlewareFile.js
@@ -28,3 +28,29 @@ exports.checkFile = (form) => {
     next();
   };
 };
+
+exports.checkUpdateFile = (form) => {
+  return async (req, res, next) => {
+    const file = req?.files?.[form];
+
+    if (file) {
+      const extension = path.extname(file.name).toLowerCase();
+      const mimeType = file.mimetype;
+
+      if (extension !== ".webp" || mimeType !== "image/webp") {
+        return sendResponse(res, 500, "Partner logo must be in WEBP format");
+      }
+
+      if (file.size > 500 * 1024) {
+        // 500KB
+        return sendResponse(
+          res,
+          500,
+          "Partner logo size is too big, please upload a file smaller than 500 KB"
+        );
+      }
+    }
+
+    next();
+  };
+};
