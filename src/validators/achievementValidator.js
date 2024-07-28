f750357e6117a298a9cd9539688329a0c130548d
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-28 17:02:32 +0700

    feat: Add achievement validation rules

---
+++ src/validators/achievementValidator.js
@@ -0,0 +1,36 @@
+const { check, validationResult } = require("express-validator");
+
+const postAchievementValidationRules = () => {
+  return [
+    check("name")
+      .isString()
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage("Achievement name is required")
+      .isLength({ max: 255 })
+      .withMessage("Achievement name must be no more than 255 characters"),
+
+    check("year")
+      .isString()
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage("Achievement year is required")
+      .isLength({ max: 4 })
+      .withMessage("Achievement year must be no more than 4 characters"),
+  ];
+};
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(500).json({ status: 500, errors: errors.array() });
+  }
+  next();
+};
+
+module.exports = {
+  postAchievementValidationRules,
+  validate,
+};
