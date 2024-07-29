7c7e2bc356c86c126027c21937ae5dad533641e0
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-29 16:06:19 +0700

    refactor: Improve abstraction and modularity in code

--- tests/controllers/ContentController.test.js
+++ tests/controllers/ContentController.test.js
@@ -34,6 +34,24 @@ describe("Content Controller", () => {
       .field("link", link);
   };
 
+  const errorMessage = async (res, statusCode, status, errorMessage) => {
+    expect(res.statusCode).toEqual(statusCode);
+    expect(res.body.status).toEqual(status);
+    expect(res.body.errors[0].msg).toBe(errorMessage);
+  };
+
+  const messageTemplate = async (res, statusCode, status, message) => {
+    expect(res.statusCode).toEqual(statusCode);
+    expect(res.body.status).toEqual(status);
+    expect(res.body.message).toBe(message);
+  };
+
+  function checkPropertiesDefined(obj, properties) {
+    properties.forEach((property) => {
+      expect(obj[property]).toBeDefined();
+    });
+  }
+
   describe("POST /api/v1/contents", () => {
     it("should be rejected if link field not https", async () => {
       const token = await authenticateUser();
@@ -42,9 +60,10 @@ describe("Content Controller", () => {
 
       const res = await createContent(token, contentData);
 
-      expect(res.statusCode).toEqual(500);
-      expect(res.body.status).toEqual(500);
-      expect(res.body.errors[0].msg).toBe(
+      errorMessage(
+        res,
+        500,
+        500,
         "The link must be a string and a link evidenced by https:// at the beginning"
       );
     }, 60000);
@@ -55,17 +74,13 @@ describe("Content Controller", () => {
 
       const res = await createContent(token, contentData);
 
-      expect(res.statusCode).toEqual(500);
-      expect(res.body.status).toEqual(500);
-      expect(res.body.errors[0].msg).toBe("The link must valid a url");
+      errorMessage(res, 500, 500, "The link must valid a url");
     }, 60000);
 
     it("should be rejected if user not authenticated", async () => {
       const res = await request(app).post("/api/v1/contents");
 
-      expect(res.statusCode).toEqual(401);
-      expect(res.body.status).toEqual(401);
-      expect(res.body.message).toBeDefined();
+      messageTemplate(res, 401, 401, "Unauthorized");
     }, 60000);
 
     it("should be able to add content", async () => {
@@ -75,9 +90,7 @@ describe("Content Controller", () => {
 
       const res = await createContent(token, contentData);
 
-      expect(res.statusCode).toEqual(200);
-      expect(res.body.status).toEqual(200);
-      expect(res.body.message).toBe("Successfully Add New Content");
+      messageTemplate(res, 200, 200, "Successfully Add New Content");
     }, 60000);
   });
 
@@ -91,14 +104,11 @@ describe("Content Controller", () => {
 
       const res = await request(app).get("/api/v1/contents");
 
-      expect(res.statusCode).toEqual(200);
-      expect(res.body.status).toEqual(200);
-      expect(res.body.message).toBe("Successfully Get All Contents");
+      messageTemplate(res, 200, 200, "Successfully Get All Contents");
+
       expect(res.body.data[0].id).toBeDefined();
-      expect(res.body.data[0].link).toBeDefined();
-      expect(res.body.data[0].created_at).toBeDefined();
-      expect(res.body.data[0].updated_at).toBeDefined();
-      expect(res.body.data[0].deleted_at).toBeDefined();
+      const properties = ["link", "created_at", "updated_at", "deleted_at"];
+      checkPropertiesDefined(res.body.data[0], properties);
     }, 60000);
   });
   describe("GET /api/v1/contents?id=id_content", () => {
@@ -114,14 +124,10 @@ describe("Content Controller", () => {
         `/api/v1/contents?id=${content.body.data[0].id}`
       );
 
-      expect(res.statusCode).toEqual(200);
-      expect(res.body.status).toEqual(200);
-      expect(res.body.message).toBe("Successfully Get All Contents");
+      messageTemplate(res, 200, 200, "Successfully Get All Contents");
       expect(res.body.data[0].id).toBeDefined();
-      expect(res.body.data[0].link).toBeDefined();
-      expect(res.body.data[0].created_at).toBeDefined();
-      expect(res.body.data[0].updated_at).toBeDefined();
-      expect(res.body.data[0].deleted_at).toBeDefined();
+      const properties = ["link", "created_at", "updated_at", "deleted_at"];
+      checkPropertiesDefined(res.body.data[0], properties);
     }, 60000);
   });
 
@@ -129,9 +135,7 @@ describe("Content Controller", () => {
     it("should be rejected if user not authenticated", async () => {
       const res = await request(app).post("/api/v1/contents");
 
-      expect(res.statusCode).toEqual(401);
-      expect(res.body.status).toEqual(401);
-      expect(res.body.message).toBeDefined();
+      messageTemplate(res, 401, 401, "Unauthorized");
     }, 60000);
 
     it("should be rejected if id params null", async () => {
@@ -147,9 +151,7 @@ describe("Content Controller", () => {
         .set("Authorization", `Bearer ${token}`)
         .send({ link: "https://blablabla.com" });
 
-      expect(res.statusCode).toEqual(404);
-      expect(res.body.status).toEqual(404);
-      expect(res.body.message).toBe("Content Not Found");
+      messageTemplate(res, 404, 404, "Content Not Found");
     }, 60000);
 
     it("should be rejected if link is not http", async () => {
@@ -165,9 +167,10 @@ describe("Content Controller", () => {
         .set("Authorization", `Bearer ${token}`)
         .send({ link: "http://safljsal" });
 
-      expect(res.statusCode).toEqual(500);
-      expect(res.body.status).toEqual(500);
-      expect(res.body.errors[0].msg).toBe(
+      errorMessage(
+        res,
+        500,
+        500,
         "The link must be a string and a link evidenced by https:// at the beginning"
       );
     }, 60000);
@@ -184,9 +187,7 @@ describe("Content Controller", () => {
         .set("Authorization", `Bearer ${token}`)
         .send({ link: "https://safljsal" });
 
-      expect(res.statusCode).toEqual(500);
-      expect(res.body.status).toEqual(500);
-      expect(res.body.errors[0].msg).toBe("The link must valid a url");
+      errorMessage(res, 500, 500, "The link must valid a url");
     }, 60000);
 
     it("should be reject cause content not found", async () => {
@@ -202,9 +203,7 @@ describe("Content Controller", () => {
         .set("Authorization", `Bearer ${token}`)
         .send({ link: "https://dewii.com" });
 
-      expect(res.statusCode).toEqual(404);
-      expect(res.body.status).toEqual(404);
-      expect(res.body.message).toBe("Content Not Found");
+      messageTemplate(res, 404, 404, "Content Not Found");
     }, 60000);
 
     it("should be able to edit content", async () => {
@@ -220,21 +219,17 @@ describe("Content Controller", () => {
         .set("Authorization", `Bearer ${token}`)
         .send({ link: "https://dewii.com" });
 
-      expect(res.statusCode).toEqual(200);
-      expect(res.body.status).toEqual(200);
-      expect(res.body.data[0].link).toBeDefined();
-      expect(res.body.data[0].created_at).toBeDefined();
-      expect(res.body.data[0].updated_at).toBeDefined();
-      expect(res.body.data[0].deleted_at).toBeDefined();
+      messageTemplate(res, 200, 200, "Successfully Update Content");
+
+      const properties = ["link", "created_at", "updated_at", "deleted_at"];
+      checkPropertiesDefined(res.body.data[0], properties);
     }, 60000);
   });
   describe("DELETE /api/v1/contents?id=id_content", () => {
     it("should be rejected if user not authenticated", async () => {
       const res = await request(app).post("/api/v1/contents");
 
-      expect(res.statusCode).toEqual(401);
-      expect(res.body.status).toEqual(401);
-      expect(res.body.message).toBeDefined();
+      messageTemplate(res, 401, 401, "Unauthorized");
     }, 60000);
 
     it("should be rejected if id params null", async () => {
@@ -249,9 +244,7 @@ describe("Content Controller", () => {
         .set("Cookie", `token=${token}`)
         .set("Authorization", `Bearer ${token}`);
 
-      expect(res.statusCode).toEqual(404);
-      expect(res.body.status).toEqual(404);
-      expect(res.body.message).toBe("Content Not Found");
+      messageTemplate(res, 404, 404, "Content Not Found");
     }, 60000);
     it("should be able to delete content", async () => {
       const token = await authenticateUser();
@@ -266,9 +259,7 @@ describe("Content Controller", () => {
         .set("Authorization", `Bearer ${token}`)
         .send({ link: "https://blablabla.com" });
 
-      expect(res.statusCode).toEqual(200);
-      expect(res.body.status).toEqual(200);
-      expect(res.body.message).toBe("Successfully Delete Content");
+      messageTemplate(res, 200, 200, "Successfully Delete Content");
     }, 60000);
   });
 });
