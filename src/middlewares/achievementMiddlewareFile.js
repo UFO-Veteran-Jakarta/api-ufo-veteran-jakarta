ac7a2b6f0f3793d5061ff4922e2e787516e50af0
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-29 11:23:19 +0700

    refactor: Improve file abstraction

--- src/middlewares/achievementMiddlewareFile.js
+++ src/middlewares/achievementMiddlewareFile.js
@@ -1,31 +1,49 @@
 const { sendResponse } = require("../helpers/response");
 const path = require("path");
 
+const validateFilePresence = (file, res) => {
+  if (!file) {
+    sendResponse(res, 500, "Achievement logo are required");
+    return false;
+  }
+  return true;
+};
+
+const validateFileFormat = (file, res) => {
+  const extension = path.extname(file.name).toLowerCase();
+  const mimeType = file.mimetype;
+
+  if (extension !== ".webp" || mimeType !== "image/webp") {
+    sendResponse(res, 500, "Achievement logo must be in WEBP format");
+    return false;
+  }
+  return true;
+};
+
+const validateFileSize = (file, res) => {
+  if (file.size > 500 * 1024) {
+    // 500KB
+    sendResponse(
+      res,
+      500,
+      "Achievement logo size is too big, please upload a file smaller than 500 KB"
+    );
+    return false;
+  }
+  return true;
+};
+
 exports.checkFile = (form) => {
   return async (req, res, next) => {
     const file = req?.files?.[form];
 
-    if (!file) {
-      return sendResponse(res, 500, "Achievement logo are required");
+    if (
+      validateFilePresence(file, res) &&
+      validateFileFormat(file, res) &&
+      validateFileSize(file, res)
+    ) {
+      next();
     }
-
-    const extension = path.extname(file.name).toLowerCase();
-    const mimeType = file.mimetype;
-
-    if (extension !== ".webp" || mimeType !== "image/webp") {
-      return sendResponse(res, 500, "Achievement logo must be in WEBP format");
-    }
-
-    if (file.size > 500 * 1024) {
-      // 500KB
-      return sendResponse(
-        res,
-        500,
-        "Achievement logo size is too big, please upload a file smaller than 500 KB"
-      );
-    }
-
-    next();
   };
 };
 
@@ -34,27 +52,11 @@ exports.checkUpdateFile = (form) => {
     const file = req?.files?.[form];
 
     if (file) {
-      const extension = path.extname(file.name).toLowerCase();
-      const mimeType = file.mimetype;
-
-      if (extension !== ".webp" || mimeType !== "image/webp") {
-        return sendResponse(
-          res,
-          500,
-          "Achievement logo must be in WEBP format"
-        );
+      if (validateFileFormat(file, res) && validateFileSize(file, res)) {
+        next();
       }
-
-      if (file.size > 500 * 1024) {
-        // 500KB
-        return sendResponse(
-          res,
-          500,
-          "Achievement logo size is too big, please upload a file smaller than 500 KB"
-        );
-      }
+    } else {
+      next();
     }
-
-    next();
   };
 };
