b5aef04af932c7eaa49c77e3415ccb89a1d267e7
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-29 17:05:01 +0700

    Refactor code for improved abstraction and modularity

--- tests/controllers/ContentController.test.js
+++ tests/controllers/ContentController.test.js
@@ -22,8 +22,7 @@ describe("Content Controller", () => {
 
   const authenticateUser = async () => {
     const login = await registerAndLogin(TEST_USERNAME, TEST_PASSWORD);
-    const token = login.body.authorization.token;
-    return token;
+    return login.body.authorization.token;
   };
 
   const createContent = async (token, { link }) => {
@@ -34,232 +33,196 @@ describe("Content Controller", () => {
       .field("link", link);
   };
 
-  const errorMessage = async (res, statusCode, status, errorMessage) => {
+  const validateResponse = (res, statusCode, status, message) => {
+    expect(res.statusCode).toEqual(statusCode);
+    expect(res.body.status).toEqual(status);
+    if (message) expect(res.body.message).toBe(message);
+  };
+
+  const validateErrorResponse = (res, statusCode, status, errorMessage) => {
     expect(res.statusCode).toEqual(statusCode);
     expect(res.body.status).toEqual(status);
     expect(res.body.errors[0].msg).toBe(errorMessage);
   };
 
-  const messageTemplate = async (res, statusCode, status, message) => {
-    expect(res.statusCode).toEqual(statusCode);
-    expect(res.body.status).toEqual(status);
-    expect(res.body.message).toBe(message);
-  };
-
-  function checkPropertiesDefined(obj, properties) {
+  const validatePropertiesDefined = (obj, properties) => {
     properties.forEach((property) => {
       expect(obj[property]).toBeDefined();
     });
-  }
+  };
 
   describe("POST /api/v1/contents", () => {
     it("should be rejected if link field not https", async () => {
       const token = await authenticateUser();
-
       const contentData = { link: "http://blablabla.com" };
-
       const res = await createContent(token, contentData);
-
-      errorMessage(
+      validateErrorResponse(
         res,
         500,
         500,
         "The link must be a string and a link evidenced by https:// at the beginning"
       );
     }, 60000);
+
     it("should be rejected if link field not valid url", async () => {
       const token = await authenticateUser();
-
       const contentData = { link: "https://bla" };
-
       const res = await createContent(token, contentData);
-
-      errorMessage(res, 500, 500, "The link must valid a url");
+      validateErrorResponse(res, 500, 500, "The link must valid a url");
     }, 60000);
 
     it("should be rejected if user not authenticated", async () => {
       const res = await request(app).post("/api/v1/contents");
-
-      messageTemplate(res, 401, 401, "Unauthorized");
+      validateResponse(res, 401, 401, "Unauthorized");
     }, 60000);
 
     it("should be able to add content", async () => {
       const token = await authenticateUser();
-
       const contentData = { link: "https://blablabla.com" };
-
       const res = await createContent(token, contentData);
-
-      messageTemplate(res, 200, 200, "Successfully Add New Content");
+      validateResponse(res, 200, 200, "Successfully Add New Content");
     }, 60000);
   });
 
   describe("GET /api/v1/contents", () => {
     it("should be able get contents", async () => {
       const token = await authenticateUser();
-
       const contentData = { link: "https://blablabla.com" };
-
       await createContent(token, contentData);
-
       const res = await request(app).get("/api/v1/contents");
-
-      messageTemplate(res, 200, 200, "Successfully Get All Contents");
-
-      expect(res.body.data[0].id).toBeDefined();
-      const properties = ["link", "created_at", "updated_at", "deleted_at"];
-      checkPropertiesDefined(res.body.data[0], properties);
+      validateResponse(res, 200, 200, "Successfully Get All Contents");
+      validatePropertiesDefined(res.body.data[0], [
+        "link",
+        "created_at",
+        "updated_at",
+        "deleted_at",
+      ]);
     }, 60000);
   });
+
   describe("GET /api/v1/contents?id=id_content", () => {
     it("should be able get contents by id", async () => {
       const token = await authenticateUser();
-
       const contentData = { link: "https://blablabla.com" };
-
       await createContent(token, contentData);
-
       const content = await request(app).get("/api/v1/contents");
       const res = await request(app).get(
         `/api/v1/contents?id=${content.body.data[0].id}`
       );
-
-      messageTemplate(res, 200, 200, "Successfully Get All Contents");
-      expect(res.body.data[0].id).toBeDefined();
-      const properties = ["link", "created_at", "updated_at", "deleted_at"];
-      checkPropertiesDefined(res.body.data[0], properties);
+      validateResponse(res, 200, 200, "Successfully Get All Contents");
+      validatePropertiesDefined(res.body.data[0], [
+        "link",
+        "created_at",
+        "updated_at",
+        "deleted_at",
+      ]);
     }, 60000);
   });
 
   describe("PUT /api/v1/contents?id=id_content", () => {
     it("should be rejected if user not authenticated", async () => {
       const res = await request(app).post("/api/v1/contents");
-
-      messageTemplate(res, 401, 401, "Unauthorized");
+      validateResponse(res, 401, 401, "Unauthorized");
     }, 60000);
 
     it("should be rejected if id params null", async () => {
       const token = await authenticateUser();
-
       const contentData = { link: "https://blablabla.com" };
-
       await createContent(token, contentData);
-
       const res = await request(app)
         .put(`/api/v1/contents?id=`)
         .set("Cookie", `token=${token}`)
         .set("Authorization", `Bearer ${token}`)
         .send({ link: "https://blablabla.com" });
-
-      messageTemplate(res, 404, 404, "Content Not Found");
+      validateResponse(res, 404, 404, "Content Not Found");
     }, 60000);
 
     it("should be rejected if link is not http", async () => {
       const token = await authenticateUser();
-
       const contentData = { link: "https://blablabla.com" };
-
       const content = await createContent(token, contentData);
-
       const res = await request(app)
         .put(`/api/v1/contents?id=${content.body.data[0].id}`)
         .set("Cookie", `token=${token}`)
         .set("Authorization", `Bearer ${token}`)
         .send({ link: "http://safljsal" });
-
-      errorMessage(
+      validateErrorResponse(
         res,
         500,
         500,
         "The link must be a string and a link evidenced by https:// at the beginning"
       );
     }, 60000);
+
     it("should be rejected if link is not valid url", async () => {
       const token = await authenticateUser();
-
       const contentData = { link: "https://blablabla.com" };
-
       const content = await createContent(token, contentData);
-
       const res = await request(app)
         .put(`/api/v1/contents?id=${content.body.data[0].id}`)
         .set("Cookie", `token=${token}`)
         .set("Authorization", `Bearer ${token}`)
         .send({ link: "https://safljsal" });
-
-      errorMessage(res, 500, 500, "The link must valid a url");
+      validateErrorResponse(res, 500, 500, "The link must valid a url");
     }, 60000);
 
-    it("should be reject cause content not found", async () => {
+    it("should be rejected because content not found", async () => {
       const token = await authenticateUser();
-
       const contentData = { link: "https://blablabla.com" };
-
       await createContent(token, contentData);
-
       const res = await request(app)
         .put(`/api/v1/contents?id=asdf`)
         .set("Cookie", `token=${token}`)
         .set("Authorization", `Bearer ${token}`)
         .send({ link: "https://dewii.com" });
-
-      messageTemplate(res, 404, 404, "Content Not Found");
+      validateResponse(res, 404, 404, "Content Not Found");
     }, 60000);
 
     it("should be able to edit content", async () => {
       const token = await authenticateUser();
-
       const contentData = { link: "https://blablabla.com" };
-
       const content = await createContent(token, contentData);
-
       const res = await request(app)
         .put(`/api/v1/contents?id=${content.body.data[0].id}`)
         .set("Cookie", `token=${token}`)
         .set("Authorization", `Bearer ${token}`)
         .send({ link: "https://dewii.com" });
-
-      messageTemplate(res, 200, 200, "Successfully Update Content");
-
-      const properties = ["link", "created_at", "updated_at", "deleted_at"];
-      checkPropertiesDefined(res.body.data[0], properties);
+      validateResponse(res, 200, 200, "Successfully Update Content");
+      validatePropertiesDefined(res.body.data[0], [
+        "link",
+        "created_at",
+        "updated_at",
+        "deleted_at",
+      ]);
     }, 60000);
   });
+
   describe("DELETE /api/v1/contents?id=id_content", () => {
     it("should be rejected if user not authenticated", async () => {
       const res = await request(app).post("/api/v1/contents");
-
-      messageTemplate(res, 401, 401, "Unauthorized");
+      validateResponse(res, 401, 401, "Unauthorized");
     }, 60000);
 
     it("should be rejected if id params null", async () => {
       const token = await authenticateUser();
-
       const contentData = { link: "https://blablabla.com" };
-
       await createContent(token, contentData);
-
       const res = await request(app)
         .delete(`/api/v1/contents?id=`)
         .set("Cookie", `token=${token}`)
         .set("Authorization", `Bearer ${token}`);
-
-      messageTemplate(res, 404, 404, "Content Not Found");
+      validateResponse(res, 404, 404, "Content Not Found");
     }, 60000);
+
     it("should be able to delete content", async () => {
       const token = await authenticateUser();
-
       const contentData = { link: "https://blablabla.com" };
-
       const content = await createContent(token, contentData);
-
       const res = await request(app)
         .delete(`/api/v1/contents?id=${content.body.data[0].id}`)
         .set("Cookie", `token=${token}`)
-        .set("Authorization", `Bearer ${token}`)
-        .send({ link: "https://blablabla.com" });
-
-      messageTemplate(res, 200, 200, "Successfully Delete Content");
+        .set("Authorization", `Bearer ${token}`);
+      validateResponse(res, 200, 200, "Successfully Delete Content");
     }, 60000);
   });
 });
