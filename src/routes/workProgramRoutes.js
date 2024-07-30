a7ddc0b0ead3d6b75da2be86f6ace207f16cf9a3
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-30 09:29:27 +0700

    feat: Add route for adding work program with image file validation

---
+++ src/routes/workProgramRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const workProgramController = require("../controllers/workProgramController");
+
+const {
+  postWorkProramValidationRules,
+  validate,
+} = require("../validators/workProgramValidator");
+const { authentication } = require("../middlewares/authMiddleware");
+const {
+  checkFile,
+  checkFileForUpdate,
+} = require("../middlewares/workProgramMiddlewareFile");
+
+const router = express.Router();
+
+router.post(
+  "/",
+  authentication(),
+  checkFile("image"),
+  postWorkProramValidationRules(),
+  validate,
+  workProgramController.addWorkProgram
+);
+
+module.exports = router;
