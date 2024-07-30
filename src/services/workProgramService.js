a7e89ed2ae24b3863192052ee8c8fb1ece028cc0
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-30 08:57:37 +0700

    feat: Update workProgramService to include getAllWorkPrograms method

--- src/services/workProgramService.js
+++ src/services/workProgramService.js
@@ -1,5 +1,17 @@
-const { addWorkProgram } = require("src/models/workPorgramModel");
+const {
+  addWorkProgram,
+  getAllWorkProgramas,
+} = require("src/models/workPorgramModel");
 
 exports.addWorkProgram = async (data) => {
   return await addWorkProgram(data);
+};
+
+exports.getAllWorkPrograms = async () => {
+  try {
+    return await getAllWorkProgramas();
+  } catch (error) {
+    console.error("Error fetching work programs: ", error);
+    throw error;
+  }
 };
