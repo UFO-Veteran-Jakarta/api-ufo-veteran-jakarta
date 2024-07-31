0938b8818cd6fd9bc5b644d232bd585958f65867
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-31 10:57:31 +0700

    feat: Add middleware for latest activity image validation
    Implemented middleware to validate latest activity image uploads, ensuring
    they are in the correct format and size.
    
    - Created validateFile function to check image format and size
    - Added checkFile middleware to validate image presence, format, and size
      for create operations
    - Added checkFileForUpdate middleware to validate image format and size
      for update operations
    
    This update ensures that only valid images are uploaded for latest activities,
    improving data quality and user experience.

---
+++ src/middlewares/latestActivityMiddlewareFile.js
@@ -0,0 +1,53 @@
+const sharp = require('sharp');
+const { sendResponse } = require('../helpers/response');
+const path = require('path');
+
+function validateFile(file, res) {
+  const extension = path.extname(file.name).toLowerCase();
+  const mimeType = file.mimetype;
+
+  if (extension !== '.webp' || mimeType !== 'image/webp') {
+    sendResponse(res, 500, 'Latest activity image must be in WEBP Format');
+    return false;
+  }
+
+  if (file.size > 500 * 1024) {
+    // 500KB
+    sendResponse(
+      res,
+      500,
+      'Latest activity image size is too big, please upload a file smaller than 500 KB',
+    );
+    return false;
+  }
+
+  return true;
+}
+
+exports.checkFile = (form) => {
+  return async (req, res, next) => {
+    const file = req?.files?.[form];
+
+    if (!file) {
+      return sendResponse(res, 500, 'Latest activity image is required');
+    }
+
+    if (!validateFile(file, res)) {
+      return;
+    }
+
+    next();
+  };
+};
+
+exports.checkFileForUpdate = (form) => {
+  return async (req, res, next) => {
+    const file = req?.files?.[form];
+
+    if (file && !validateFile(file, res)) {
+      return;
+    }
+
+    next();
+  };
+};
