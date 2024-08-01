74adca93c6ed3b9bbe1ba2dd597c3ae2359ff709
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-08-02 06:58:11 +0700

    feat: Add division service functions
    Implemented service functions for division operations, including:
    - addDivision: Adds a new division.
    - getAllDivisions: Retrieves all divisions.
    - getDivisionById: Fetches a division by ID.
    - updateDivisionById: Updates a division by ID.
    - deleteDivisionById: Deletes a division by ID.
    
    These service functions provide a layer of abstraction over the
    division model functions, ensuring clean separation of concerns and
    better maintainability.

---
+++ src/services/divisionSerice.js
@@ -0,0 +1,31 @@
+const {
+  addDivision,
+  getAllDivisions,
+  getDivisionById,
+  updateDivisionById,
+  deleteDivisionById,
+} = require('../models/divisionModel');
+
+exports.addDivision = async (data) => {
+  return addDivision(data);
+};
+
+exports.getAllDivisions = async () => {
+  try {
+    return await getAllDivisions();
+  } catch (error) {
+    console.error('Error fetching divisions: ', error);
+  }
+};
+
+exports.getLatestDivisionById = async (id) => {
+  return getDivisionById(id);
+};
+
+exports.updateDivisionById = async (id, data) => {
+  return updateDivisionById(id, data);
+};
+
+exports.deleteDivisionById = async (id) => {
+  return deleteDivisionById(id);
+};
