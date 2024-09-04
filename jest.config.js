fc058b3f8091007412d3b9695902926ff8e4c04b
Muh11 <naufalaswinana@gmail.com>
2024-09-04 20:12:20 +0700

    feat: add configuration for jest config

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,13 @@ module.exports = {
   moduleNameMapper: {
     '^src/(.*)': '<rootDir>/src/$1',
   },
-
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
   moduleDirectories: ['node_modules', 'src'],
 };
