b605c2b6c272d07850b859987f2c6384197f5d61
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-09-17 03:47:39 +0700

    chore: Update coverage thresholds to 85% for branches, functions, lines, and statements in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,10 @@ module.exports = {
   },
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85,
     },
   },
   moduleDirectories: ['node_modules', 'src'],
