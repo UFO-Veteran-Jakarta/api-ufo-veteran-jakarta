c33696f846fba8b40f8c0221a52f6feb50bf4086
Muh11 <naufalaswinana@gmail.com>
2024-10-21 13:12:49 +0700

    feat: create helper for slug division

--- src/helpers/slug.js
+++ src/helpers/slug.js
@@ -3,6 +3,19 @@ const crypto = require('crypto');
 
 exports.generateUniqueSlug = (title) => {
   const baseSlug = slugify(title, { lower: true, strict: true });
-  const uniqueCode = crypto.randomBytes(4).toString('hex'); // Menghasilkan 8 karakter acak
+  const uniqueCode = crypto.randomBytes(4).toString('hex'); 
+  return `${baseSlug}-${uniqueCode}`;
+};
+
+exports.createSlugDivision = async (title, checkSlugExistsInDb) => {
+  const baseSlug = slugify(title, { lower: true, strict: true });
+
+  const exists = await checkSlugExistsInDb(baseSlug);
+
+  if (!exists) {
+    return baseSlug; 
+  }
+
+  const uniqueCode = crypto.randomBytes(4).toString('hex');
   return `${baseSlug}-${uniqueCode}`;
 };
