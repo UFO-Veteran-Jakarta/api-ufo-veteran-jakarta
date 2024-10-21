412089f33f7d1042210214dc7445893d6869c798
Muh11 <naufalaswinana@gmail.com>
2024-10-21 13:11:34 +0700

    feat: create util for Division image file upload
    - This util using express-file-upload package to save uploaded Division image to 'public/images/divisons/'

--- src/utils/uploadFileDivision.js
+++ src/utils/uploadFileDivision.js
@@ -1,20 +1,34 @@
 const path = require('path');
 const fs = require('fs');
-const { generateRandomFilename } = require('../helpers/slug');
+const crypto = require('crypto');
 
 const uploadFileDivision = (file) => {
   if (!file) return null;
 
   const uploadDir = './public/images/divisions/';
+
   if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
   }
 
-  const randomFilename = generateRandomFilename(file.originalname);
+const ext = path.extname(file.name);
+
+  const generateRandomFilename = () => {
+    return crypto
+      .randomBytes(12) 
+      .toString('base64')
+      .replace(/\+/g, 'A')
+      .replace(/\//g, 'B')
+      .replace(/=+$/, '')
+      .substring(0, 16);
+  };
+
+const randomFilename = generateRandomFilename() + ext;
+
   const filePath = path.join(uploadDir, randomFilename);
 
-  fs.writeFileSync(filePath, file.buffer);
-  return `/images/divisions/${randomFilename}`;
-};
+  fs.writeFileSync(filePath, file.data); 
+  return `/images/divisions/${randomFilename}`; 
 
+};
 module.exports = uploadFileDivision;
