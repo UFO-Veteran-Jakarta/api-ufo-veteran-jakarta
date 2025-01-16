96464c950604c472ba779d26cf02c62897857215
Haikal Rafif <99420525+haikalrafifas@users.noreply.github.com>
2025-01-16 05:47:48 +0000

    hotfix: updating legacy code to be compatible with the new data flow.
    This commit ensures that there will be no more integration error since version 1.0.0.

--- src/models/categoriesModel.js
+++ src/models/categoriesModel.js
@@ -22,7 +22,7 @@ exports.addCategories = async (data) => {
 };
 
 exports.getAllCategories = async () => {
-  const query = 'SELECT * FROM myschema.categories WHERE deleted_at IS NULL;';
+  const query = 'SELECT * FROM categories WHERE deleted_at IS NULL;';
   try {
     const res = await pool.query(query);
     return res.rows;
@@ -34,13 +34,13 @@ exports.getAllCategories = async () => {
 
 exports.getCategoriesById = async (id) => {
   const query =
-  'SELECT * FROM myschema.categories WHERE id = $1 AND deleted_at IS NULL;';
+  'SELECT * FROM categories WHERE id = $1 AND deleted_at IS NULL;';
   const values = [id];
   return executeQuery(query, values);
 };
 
 exports.deleteAllCategories = async () => {
-  const query = 'UPDATE myschema.categories SET deleted_at = NOW()';
+  const query = 'UPDATE categories SET deleted_at = NOW()';
   try {
     const res = await pool.query(query);
     return res.rows;
@@ -62,7 +62,7 @@ exports.updateCategoriesById = async (id, data) => {
 
 exports.deleteCategoriesById = async (id) => {
   const query =
-    'UPDATE myschema.categories SET deleted_at = NOW() WHERE id = $1 RETURNING *;';
+    'UPDATE categories SET deleted_at = NOW() WHERE id = $1 RETURNING *;';
   const values = [id];
   return executeQuery(query, values);
 };
