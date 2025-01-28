369911872c49715f73595fd98bc0fff50b4d7de8
Muh11 <naufalaswinana@gmail.com>
2025-01-28 17:03:25 +0700

    feat: create update validation rules and data validation rules

--- src/validators/eventValidator.js
+++ src/validators/eventValidator.js
@@ -99,17 +99,143 @@ const postValidationRules = () => {
   ];
 };
 
+const updateValidationRules = () => {
+  return [
+    check('name')
+      .optional()
+      .isString()
+      .trim()
+      .isLength({ max: 255 })
+      .withMessage('Event name must be no more than 255 characters'),
+
+    check('start_event_date')
+      .optional()
+      .isISO8601()
+      .toDate()
+      .withMessage('Start event date must be a date and with format DDMMYYYY'),
+
+    check('registration_start_date')
+      .optional()
+      .isISO8601()
+      .toDate()
+      .withMessage(
+        'Start registration date must be a date and with format DDMMYYYY',
+      ),
+
+    check('registration_end_date')
+      .optional()
+      .isISO8601()
+      .toDate()
+      .withMessage(
+        'End registration date must be a date and with format DDMMYYYY',
+      ),
+
+    check('end_event_date')
+      .optional()
+      .isISO8601()
+      .toDate()
+      .withMessage('End event date must be a date and with format DDMMYYYY'),
+
+    check('start_event_time')
+      .optional()
+      .matches(/^(?:[01]\d|2[0-3])[0-5]\d$/)
+      .withMessage('Start event time must be a time and with format HHMM'),
+
+    check('end_event_time')
+      .optional()
+      .matches(/^(?:[01]\d|2[0-3])[0-5]\d$/)
+      .withMessage('End event time must be a time and with format HHMM'),
+
+    check('registration_start_time')
+      .optional()
+      .matches(/^(?:[01]\d|2[0-3])[0-5]\d$/)
+      .withMessage(
+        'Start registration time must be a time and with format HHMM',
+      ),
+
+    check('registration_end_time')
+      .optional()
+      .matches(/^(?:[01]\d|2[0-3])[0-5]\d$/)
+      .withMessage('End registration time must be a time and with format HHMM'),
+
+    check('body')
+      .optional()
+      .isString()
+      .withMessage('Body must be a string when provided'),
+
+    check('snippets')
+      .optional()
+      .escape()
+      .isString()
+      .custom((value) => {
+        if (value && value.split(' ').length > 20) {
+          throw new Error("The snippets can't be longer than 20 words");
+        }
+        return true;
+      }),
+
+    check('link_registration')
+      .optional()
+      .isString()
+      .trim()
+      .matches(/^https:\/\/.*/)
+      .withMessage('The registration link must start with https://'),
+
+    check('location')
+      .optional()
+      .escape()
+      .isString()
+      .withMessage('Location must be a string when provided'),
+  ];
+};
+
+// Optional: Added custom date validation middleware
+const validateDateConsistency = (req, res, next) => {
+  const {
+    start_event_date,
+    end_event_date,
+    registration_start_date,
+    registration_end_date,
+  } = req.body;
+
+  if (start_event_date && end_event_date) {
+    if (new Date(start_event_date) > new Date(end_event_date)) {
+      return res.status(400).json({
+        errors: [
+          {
+            msg: 'Start event date must be before end event date',
+          },
+        ],
+      });
+    }
+  }
+
+  if (registration_start_date && registration_end_date) {
+    if (new Date(registration_start_date) > new Date(registration_end_date)) {
+      return res.status(400).json({
+        errors: [
+          {
+            msg: 'Registration start date must be before registration end date',
+          },
+        ],
+      });
+    }
+  }
+
+  next();
+};
+
 const validate = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(500).json({ status: 500, errors: errors.array() });
   }
-
-  req.body = matchedData(req, { onlyValidData: true });
   next();
 };
 
 module.exports = {
   postValidationRules,
   validate,
+  updateValidationRules,
+  validateDateConsistency
 };
