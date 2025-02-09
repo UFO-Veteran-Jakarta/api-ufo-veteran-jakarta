c2632d816b75e281dd1cf7d0d5ad420b9a16cc7d
haikalrafifas <haikalrafifas@gmail.com>
2025-02-09 12:53:59 +0700

    feat: implement headless browser for scraping webpage
    # Changes
    - Changed `src/utils/scraper.js` to implement puppeteer headless browser to fully render target site.
    - Changed `.env.example` and `src/config/scraper.js` to support headless browser configuration.

--- src/config/scraper.js
+++ src/config/scraper.js
@@ -1,5 +1,6 @@
 // HTML scraper configuration
 module.exports = {
-  baseUrl: 'https://ufoveteran.com',
+  baseUrl: process.env.SCRAPER_BASE_URL || 'https://ufoveteran.com',
   ttl: 60 * 60 * 1000, // minutes * seconds * ms = 1 hour
+  browserExecutable: process.env.SCRAPER_BROWSER_EXECUTABLE || 'C:/Program Files/Google/Chrome/Application/chrome.exe',
 };
